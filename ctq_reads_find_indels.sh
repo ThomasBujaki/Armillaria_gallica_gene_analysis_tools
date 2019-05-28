c17ec28b74c81a16c8b8a8ddc8ac630c08518964
kassandradickson <kassandradickson@cmail.carleton.ca>
2019-05-28 13:43:53 -0400

    Changed the strainNumber so it would run properly

--- ctq_reads_find_indels.sh
+++ ctq_reads_find_indels.sh
@@ -16,15 +16,15 @@ strainNumber=$1
 
 function mpileup {
 
-samtools mpileup -d 250 -m 1 -E --BCF --output-tags DP,AD,ADF,SP -f Armga1_AssemblyScaffolds.fasta -o Ar$strainNumber_ctq_reads_indels.bcf Ar$strainNumber_ctq_reads.bam
+samtools mpileup -d 250 -m 1 -E --BCF --output-tags DP,AD,ADF,SP -f Armga1_AssemblyScaffolds.fasta -o Ar${strainNumber}_ctq_reads_indels.bcf Ar${strainNumber}_ctq_reads.bam
 }
 
 function indexing {
-bcftools index  Ar$strainNumber_ctq_reads_indels.bcf
+bcftools index  Ar${strainNumber}_ctq_reads_indels.bcf
 }
 
 function call {
-bcftools call --skip-variants snps --multiallelic-caller --variants-only  -O v Ar$strainNumber_ctq_reads_indels.bcf -o Ar$strainNumber_ctq_reads_indels.vcf
+bcftools call --skip-variants snps --multiallelic-caller --variants-only  -O v Ar${strainNumber}_ctq_reads_indels.bcf -o Ar${strainNumber}_ctq_reads_indels.vcf
 }
 
 #Main
