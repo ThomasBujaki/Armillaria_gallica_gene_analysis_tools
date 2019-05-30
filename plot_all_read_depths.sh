dc02d3815a8f45085dc4e8714022fa36d32fe2c4
Thomas Bujaki <ofbujak@gmail.com>
2019-05-30 16:16:16 -0400

    changed plot all read depths to path to a local binary of the Rscript program

--- plot_all_read_depths.sh
+++ plot_all_read_depths.sh
@@ -3,7 +3,7 @@ sequence=$1
 i=0;
 while IFS='' read -r line || [[ -n "$line" ]]; do
     ./print_read_depth.out read_depth_Ar109_ctq_reads.bam $line > ${i}_${sequence}
-    /usr/bin/Rscript $(pwd)/plot_read_depth.R ${i}_${sequence} $line
+    $(pwd)/Rscript $(pwd)/plot_read_depth.R ${i}_${sequence} $line
     mv ${i}_${sequence}_read_depth.pdf read_depth_graphs/$sequence/
     rm ${i}_${sequence}
     i=$((i+1))
