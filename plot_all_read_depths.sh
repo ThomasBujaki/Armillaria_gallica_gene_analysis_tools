c2d0502af24917311ef3f4a95784a246d8982ce1
Thomas Bujaki <ofbujak@gmail.com>
2019-05-30 16:03:34 -0400

    modified plot_all_read_depth.sh to hard path to Rscript binary

--- plot_all_read_depths.sh
+++ plot_all_read_depths.sh
@@ -3,7 +3,7 @@ sequence=$1
 i=0;
 while IFS='' read -r line || [[ -n "$line" ]]; do
     ./print_read_depth.out read_depth_Ar109_ctq_reads.bam $line > ${i}_${sequence}
-    Rscript $(pwd)/plot_read_depth.R ${i}_${sequence} $line
+    /usr/bin/Rscript $(pwd)/plot_read_depth.R ${i}_${sequence} $line
     mv ${i}_${sequence}_read_depth.pdf read_depth_graphs/$sequence/
     rm ${i}_${sequence}
     i=$((i+1))
